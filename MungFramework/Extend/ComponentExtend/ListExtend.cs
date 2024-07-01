fd4c02b2114521a9d2489c47435f973c0f1efe9c
MungRed <53520889+MungRed@users.noreply.github.com>
2024-07-01 14:42:22 +0800

    Delete MungFramework directory

--- MungFramework/Extend/ComponentExtend/ListExtend.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace MungFramework.ComponentExtend
-{
-    public static class ListExtend
-    {
-        public static bool Empty<T>(this List<T> list)=>list.Count == 0;
-    }
-}
